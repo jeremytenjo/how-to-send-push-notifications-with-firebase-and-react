4631e775dc50a28f934612b773b4394c2ae6d9e0
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-02-21 23:34:41 -0500

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { UseInstallPromptProvider } from '@useweb/use-install-prompt'
 
 import Firebase from './services/firebase/firebase'
 import Router from './pages/router'
@@ -8,13 +7,11 @@ import Theme from './theme/theme'
 
 function App() {
   return (
-    <UseInstallPromptProvider>
-      <Firebase>
-        <Theme>
-          <Router />
-        </Theme>
-      </Firebase>
-    </UseInstallPromptProvider>
+    <Firebase>
+      <Theme>
+        <Router />
+      </Theme>
+    </Firebase>
   )
 }
 
