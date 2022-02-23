179ca7c273f4db111306e43d66b221afd62b1f51
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-02-23 11:40:04 -0500

    removed @useweb/use-install-prompt

--- devtools/vite/plugins/plugins.ts
+++ devtools/vite/plugins/plugins.ts
@@ -5,7 +5,7 @@ export default async function getVitePlugins(payload: PayloadTypes) {
     await (await import('./react/react')).default(),
     await (await import('./html/html')).default(payload),
     await (await import('./compression/compression')).default(payload),
-    await (await import('./pwa/pwa')).default(payload),
+    // await (await import('./pwa/pwa')).default(payload),
     await (await import('./visualizer/visualizer')).default(payload),
   ]
 }
